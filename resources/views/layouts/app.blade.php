6e81b89879cec3e0148e0705affa9bad043d9a9b
Senioktoviani <senioktoviani148@gmail.com>
2022-03-14 09:39:56 +0700

    pendaftaran pasien

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,8 +51,6 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-
-
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
@@ -63,7 +61,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                           document.getElementById('logout-form').submit();">
+                                                                                               document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -85,7 +83,7 @@
 
         <footer class="fixed-bottom">
             <center>
-                Copyright &copy; <strong>Seni Oktoviani Dan Jojo Susanto </strong>- {{ date('Y') }}
+                Copyright &copy; <strong>Seni Oktoviani</strong>- {{ date('Y') }}
             </center>
         </footer>
     </div>
